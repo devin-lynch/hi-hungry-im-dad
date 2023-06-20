fb3eded3148759034d9b344d0064fb19ce743886
devin-lynch <devin.l.lynch@gmail.com>
2023-06-20 17:18:01 -0500

    Clean up fetchJoke code

--- app/page.tsx
+++ app/page.tsx
@@ -6,56 +6,39 @@ export default function Home() {
 
   const [joke, setJoke] = useState()
 
-  useEffect(() => {
-    const apiCall = async () => {
-      try {
+  const fetchJoke = async () => {
+    try {
         const url = 'https://icanhazdadjoke.com/'
-        const response = await fetch(url, {
+        let response = await fetch(url, {
           headers: {
             Accept: 'application/json'
           }
         })
-        const data = await response.json()
-        console.log(data)
+        let data = await response.json()
+        // console.log(data)
         return data.joke
       } catch (error) {
         console.log(error)
       }
     }
-    setJoke(apiCall())
+
+  useEffect(() => {
+    setJoke(fetchJoke())
   }, [])
 
   const handleButtonClick = () => {
-    const apiCall = async () => {
-      try {
-        const url = 'https://icanhazdadjoke.com/'
-        const response = await fetch(url, {
-          headers: {
-            Accept: 'application/json'
-          }
-        })
-        const data = await response.json()
-        console.log(data)
-        return data.joke
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    setJoke(apiCall())
+    setJoke(fetchJoke())
   }
-
 
   return (
     <main className='text-center mt-8'>
-      {joke}
-      <div>
-        <button
-          className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mt-4"
-          onClick={() => {handleButtonClick()}}
-        >
-          fetch joke
-        </button>
-      </div>
+      <p>{joke}</p>
+      <button
+        className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mt-4'
+        onClick={() => {handleButtonClick()}}
+      >
+        fetch joke
+      </button>
     </main>
   )
 }

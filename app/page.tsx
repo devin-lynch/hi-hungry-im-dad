0991797e495935ae46f268560d1e261c43556c9a
devin-lynch <devin.l.lynch@gmail.com>
2023-06-27 16:08:46 -0500

    Make second button post joke to backend

--- app/page.tsx
+++ app/page.tsx
@@ -6,39 +6,63 @@ export default function Home() {
 
   const [joke, setJoke] = useState()
 
-  const fetchJoke = async () => {
-    try {
-        const url = 'https://icanhazdadjoke.com/'
-        let response = await fetch(url, {
-          headers: {
-            Accept: 'application/json'
-          }
-        })
+    const fetchJoke = async () => {
+      try {
+        let response = await fetch('http://localhost:8000')
         let data = await response.json()
-        // console.log(data)
-        return data.joke
+        return data
       } catch (error) {
         console.log(error)
       }
     }
 
   useEffect(() => {
-    setJoke(fetchJoke())
+    fetchJoke().then(setJoke)
   }, [])
 
-  const handleButtonClick = () => {
-    setJoke(fetchJoke())
+  const handleButtonClick = async () => {
+    try {
+      setJoke(await fetchJoke())
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const stashJoke = async () => {
+    try {
+      let response = await fetch('http://localhost:8000/save', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          text: joke
+        })
+      })
+      let data = await response.json()
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
     <main className='text-center mt-8'>
-      <p>{joke}</p>
-      <button
+
+        <p>{joke}</p>
+        <button
         className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mt-4'
-        onClick={() => {handleButtonClick()}}
-      >
-        fetch joke
-      </button>
+          onClick={() => {stashJoke()}}
+        >
+          save
+        </button>
+      <div>
+        <button
+          className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mt-4'
+          onClick={() => {handleButtonClick()}}
+        >
+          fetch joke
+        </button>
+      </div>
     </main>
   )
 }

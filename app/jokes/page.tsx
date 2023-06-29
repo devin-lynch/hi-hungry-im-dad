d932f060c4db462ef6ebe8efa496b9a90c9b4177
devin-lynch <devin.l.lynch@gmail.com>
2023-06-29 16:09:57 -0500

    Show comments for each joke

--- app/jokes/page.tsx
+++ app/jokes/page.tsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from 'react'
 export default function Jokes() {
 
     const [jokes, setJokes] = useState([])
+    const [jokeElements, setjokeElements] = useState([])
+    const [comment, setComment] = useState('')
 
     useEffect(() => {
         const unleashJokes = async () => {
@@ -18,16 +20,43 @@ export default function Jokes() {
             }
         }
         unleashJokes().then(setJokes)
-        console.log(jokes)
-    }, [])
 
-    const jokies = jokes.map((joke, i) => {
-        return <li>{joke.text}</li>
-    })
+
+        const fetchComments = async (jokeId) => {
+            try {
+                let response = await fetch(`http://localhost:8000/comments/${jokeId}`)
+                let data = await response.json()
+                // console.log(data)
+                return data
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        const daJokes = jokes.map(async (joke, i)  => {
+            const allComments = await fetchComments(joke.id)
+            const jokeComments = allComments.map((comment, i) => {
+                return(
+                    <p key={i}>{comment.text}</p>
+                )
+            })
+            return (
+                    <div className='mt-2'>
+                        <li>
+                            {joke.text}
+                        </li>
+                        {jokeComments}
+                        <input type="text" name='comment' placeholder='comment'/>
+                        <input type="submit" name='comment'/>
+                    </div>
+            )
+        })
+        setjokeElements(daJokes)
+    }, [])
 
     return (
         <div className='text-center mt-6'>
-            <ul>{jokies}</ul>
+            <ul>{jokeElements}</ul>
             <div className='mt-4'>
                 <a style={{background: 'blue'}} href="http://localhost:3000/">more jokes</a>
             </div>
